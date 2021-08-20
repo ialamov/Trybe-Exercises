984e97c90d51b12b388315effe02b7d120e7947d
Douglas Ialamov <douglasialamov@MacBook-Air-de-Douglas.local>
2021-08-20 13:20:50 -0300

    Exercises of variables and dinamic.

---
+++ fundamentos/bloco-04/primeiros-passos/script.js
@@ -0,0 +1,21 @@
+const myName = "Douglas de Oliveira Ialamov";
+const birthCity = "São Paulo";
+let birthYear = 1987;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+// Constantes não podem ser alteradas, por isto não há como dar uma nova atribuição.
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log(area);
+
+const perimeter = 2 * base + 2 * height;
+
+console.log(perimeter);
